91bc8c807f007b0fa5b466c4c6566c7feeb56e74
Shiran Salem <p.tagor@gmail.com>
2019-04-05 16:58:32 +0300

    add integration tests

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
-  return (
-    <Provider store={createStore(reducers, initialState)}>
-      {/* props.children is a react construct it allows to take the component we just created and use it to wrap other components */}
-      {children}
-    </Provider>
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
   );
+
+  /* props.children is a react construct it allows to take the component we just created and use it to wrap other components */
+  return <Provider store={store}>{children}</Provider>;
 };
