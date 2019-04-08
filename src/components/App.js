698653d3044814ff9b7504430c1ed2c91e98823c
Tagor Petrosian <p.tagor@gmail.com>
2019-04-08 18:02:59 +0200

    update auth

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,30 @@ import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
 class App extends Component {
   renderButton() {
     if (this.props.auth) {
-      return <button>Sign Out</button>;
+      return (
+        <button
+          onClick={() => {
+            this.props.changeAuth(false);
+          }}
+        >
+          Sign Out
+        </button>
+      );
     } else {
-      return <button>Sign In</button>;
+      return (
+        <button
+          onClick={() => {
+            this.props.changeAuth(true);
+          }}
+        >
+          Sign In
+        </button>
+      );
     }
   }
   renderHeader() {
@@ -19,7 +36,7 @@ class App extends Component {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/">Post A Comment</Link>
+          <Link to="/post">Post A Comment</Link>
         </li>
         <li>{this.renderButton()}</li>
       </ul>
@@ -39,4 +56,7 @@ class App extends Component {
 function mapStateToProps(state) {
   return { auth: state.auth };
 }
-export default connect(mapStateToProps)(App);
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
